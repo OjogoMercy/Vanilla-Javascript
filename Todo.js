c6a46435409f6522ba9478eab2b1d40c9dae8b8f
Armykookie35 <mercyojogo@gmail.com>
2025-06-05 20:42:12 -0700

    f

--- Todo.js
+++ Todo.js
@@ -1,23 +1,24 @@
 console.log('Todo-App')
 console.log('Tasks')
 const Tasks = [
-    { id: 1, text: 'Buy groceries' ,liked:false,mark:true},
-    { id: 2, text: 'Walk the dog',liked:false,mark:true },
-    { id: 3, text: 'Read a book',liked:false ,mark:false},
+    { id: 1, text: 'Buy groceries' ,liked:false,mark:true,pending: true},
+    { id: 2, text: 'Walk the dog',liked:false,mark:false ,pending: true},
+    { id: 3, text: 'Read a book',liked:false ,mark:false,pending: true},
 ]
 function addTask (id,text,liked){
    return Tasks.push({
     id: id,
     text: text,
     liked :liked,
-    mark: false
+    mark: false,
+    pending: true
    });
 }
 
 addTask(4,'Take a break', false)
 addTask(5, 'Take a Walk', true)
 
-const toMark = Tasks.find((task) => task.id === 1);
+const toMark = Tasks.find((task) => task.id === 2);
 
 // const a = Tasks.map((d, i)=>{
 //     if (d.id === 2){
@@ -30,7 +31,7 @@ const toMark = Tasks.find((task) => task.id === 1);
 
 // to mark  a task as completed
 if (toMark){
-    toMark.mark= false;
+    toMark.mark= true;
 console.log('Task marked as done',Tasks[2].text);
    
 }    
@@ -38,5 +39,11 @@ console.log('Task marked as done',Tasks[2].text);
 let remove = Tasks.filter((task) => task.id !== 1);
 
 // filter tasks as completed or pending
+
+const toComplete = Tasks.find((tasks) => tasks.id === 4)
+if (toComplete){
+    toComplete.pending = false;
+    console.log(`Tasks marked as Completed`)
+}
 
 console.log(remove)
